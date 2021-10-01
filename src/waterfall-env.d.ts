907337ded05caa98343260ace80fb4b7b99e4ed3
HippoIvan <1090334606@qq.com>
2021-10-01 18:04:10 +0800

    updated

--- src/waterfall-env.d.ts
+++ src/waterfall-env.d.ts
@@ -10,14 +10,16 @@ declare module "@emotion/react" {
   export interface Theme extends ITheme {}
 }
 
-interface Window {
-  __REDUX_DEVTOOLS_EXTENSION__?: Function;
-  ethereum?: {
-    isMetaMask?: true;
-    request?: (...args: any[]) => Promise<void>;
-    on?: (...args: any[]) => void;
-    removeListener?: (...args: any[]) => void;
-    autoRefreshOnNetworkChange?: boolean;
-    chainId: string;
-  };
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__?: Function;
+    ethereum?: {
+      isMetaMask?: true;
+      request?: (...args: any[]) => Promise<void>;
+      on?: (...args: any[]) => void;
+      removeListener?: (...args: any[]) => void;
+      autoRefreshOnNetworkChange?: boolean;
+      chainId: string;
+    };
+  }
 }

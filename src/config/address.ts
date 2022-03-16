0c8f7bcd32bee2f278fd50d0f2f22bcf0e21e262
0xAngel <0xangel@waterfalldefi.org>
2022-03-16 11:36:18 +0800

    new addys

--- src/config/address.ts
+++ src/config/address.ts
@@ -227,49 +227,26 @@ export const MC_sALPACA_TUSDAddress: { [network: string]: string } = {
 export const AR_TrancheMasterAddress: { [network: string]: string } = {
   [NETWORKS.DEVNET]: "",
   [NETWORKS.TESTNET]: "",
-  // [NETWORKS.MAINNET]: "0x3d1c938351201267BAdf49Be50188F8F81F21A20"
-  //March 1st
-  // [NETWORKS.MAINNET]: "0xe8096E793C47Aa13Eb17A6753B0DB45cA35ECa02"
-  //March 9th
-  // [NETWORKS.MAINNET]: "0x4460c77B12476Bd6bf4cf2576011a4a68Da6Aa19"
-  //March 15th
-  [NETWORKS.MAINNET]: "0xd6fA652fb754FC3d8CDCfeaBD8C30A1b4eAecfdd"
+  [NETWORKS.MAINNET]: "0xea45435Cbd83DcBD8959B362814C5b8e3CdB0708"
 };
 
 //autoroll master WTF contract address
 export const AR_WTFMasterAddress: { [network: string]: string } = {
   [NETWORKS.DEVNET]: "",
   [NETWORKS.TESTNET]: "",
-  // [NETWORKS.MAINNET]: "0x3a8c5ee1f0Ff9Df1929b509bddc19CE7417208bf"
-  //March 1st
-  // [NETWORKS.MAINNET]: "0x63bf79762348eeDeB5EAE094837dd9DE820af073"
-  //March 9th
-  // [NETWORKS.MAINNET]: "0x69496300379ff8D09aB9aF4Cd1dcb9C8C4bb5918"
-  //March 15th
-  [NETWORKS.MAINNET]: "0x6228253083D2061567Fb191Bd09d01033401185A"
+  [NETWORKS.MAINNET]: "0x8bc5dD0EbFEC33d59E968017F7b0821D43968845"
 };
 //autoroll ALPACA strategy BUSD lending vault address
 export const AR_sALPACA_Address: { [network: string]: string } = {
   [NETWORKS.DEVNET]: "",
   [NETWORKS.TESTNET]: "",
-  // [NETWORKS.MAINNET]: "0x121b92D6300cB2830C380dda04D5470E04F515AA"
-  //March 1st
-  // [NETWORKS.MAINNET]: "0x7d01CB7DF150eBA703907dCa2cb91E37bf28c085"
-  //March 9th
-  // [NETWORKS.MAINNET]: "0x4C0179E4e4A30A9e4ae1CE348762BC3841f1AD37"
-  //March 15th
-  [NETWORKS.MAINNET]: "0xe59a01E943ee6c46C19A6ea3de8A8f4eDbDC1d85"
+  [NETWORKS.MAINNET]: "0xf3C81d0960a0C14afBf9dC252ed4Ae4486c272eD"
 };
 //autoroll VENUS strategy TUSD lending vault address
 export const AR_sVENUS_Address: { [network: string]: string } = {
   [NETWORKS.DEVNET]: "",
   [NETWORKS.TESTNET]: "",
-  // [NETWORKS.MAINNET]: "0x3fdCA46Fbc38Ed44b2464134b0E91827deaAf64D"
-  //March 1st
-  // [NETWORKS.MAINNET]: "0x2cC5EF4Cb1Ba1080813489563506539A7F100633"
-  //March 9th
-  // [NETWORKS.MAINNET]: "0x03d64d57F40BFa20841855A1be0FeC562EaA6A89"
-  [NETWORKS.MAINNET]: "0xF0aFbA8331EDdc6825f7012d4534599a4542a49C"
+  [NETWORKS.MAINNET]: "0x8c80E504dCA6eD888494714c289b65b82b3a5532"
 };
 
 export const BNBBUSD_sALPACA_Address: { [network: string]: string } = {

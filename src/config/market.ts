95ae839421a67e9ec4a269d42c0622b86d1834e9
0xAngel <0xangel@waterfalldefi.org>
2022-04-01 21:47:21 +0800

    market fix

--- src/config/market.ts
+++ src/config/market.ts
@@ -270,44 +270,43 @@ export const MarketList: Market[] = [
     isRetired: false
   },
   {
-    portfolio: "AVAX Falls (Autorolled)",
+    portfolio: "AVAX Falls (Retired)",
     isAvax: true,
     wrapAvax: true,
-    autorollImplemented: true,
+    autorollImplemented: false,
     isMulticurrency: false,
     assets: ["WAVAX"],
     tokens: [],
-    listingDate: "2022/3/29",
+    listingDate: "2022/2/28",
     tranches: [],
     tvl: "",
     totalTranchesTarget: "",
     status: "",
     nextTime: "",
-    address: AR_AVAXFallsTrancheMasterAddress[NETWORK],
-    abi: AR_AVAXTrancheMasterAbi,
+    address: WAVAXFallsTrancheMasterAddress[NETWORK],
+    abi: AVAXTranchesAbi,
     masterChefAbi: MasterChefAbi,
-    masterChefAddress: AR_AVAXFallsMasterWTFAddress[NETWORK],
+    masterChefAddress: WAVAXFallsMasterWTFAddress[NETWORK],
     pools: [],
-    depositAssetAddress: AR_AVAXDepositAddress[NETWORK],
+    depositAssetAddress: WAVAXDepositAddress[NETWORK],
     depositAssetAddresses: [],
     depositAssetAbi: WTFAbi,
     strategyFarms: [
       {
         farmName: "Benqi AVAX",
         shares: 0.5,
-        sAddress: AR_AVAXBenqiStrategyAddress[NETWORK],
+        sAddress: WAVAXBenqiStrategyAddress[NETWORK],
         apiKey: "qi_avax"
       },
       {
         farmName: "Trader Joe AVAX",
         shares: 0.5,
-        sAddress: AR_AVAXTraderJoeStrategyAddress[NETWORK],
+        sAddress: WAVAXTraderJoeStrategyAddress[NETWORK],
         apiKey: "joe_avax"
       }
     ],
-    //incoming
-    subgraphURL: "https://apitest.waterfalldefi.org/subgraphs/name/waterfall/avax_test_qiJoe_avax",
-    isRetired: false
+    subgraphURL: "https://api3.waterfalldefi.org/subgraphs/name/waterfall/qiJoe_avax",
+    isRetired: true
   },
   {
     portfolio: "BUSD Falls (Autorolled)",

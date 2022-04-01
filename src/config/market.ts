f9d09c77acce25ad3790aabda3243e34da67af4c
0xAngel <0xangel@waterfalldefi.org>
2022-04-01 22:05:02 +0800

    remove duplicate fall

--- src/config/market.ts
+++ src/config/market.ts
@@ -270,45 +270,6 @@ export const MarketList: Market[] = [
     isRetired: false
   },
   {
-    portfolio: "AVAX Falls (Retired)",
-    isAvax: true,
-    wrapAvax: true,
-    autorollImplemented: false,
-    isMulticurrency: false,
-    assets: ["WAVAX"],
-    tokens: [],
-    listingDate: "2022/2/28",
-    tranches: [],
-    tvl: "",
-    totalTranchesTarget: "",
-    status: "",
-    nextTime: "",
-    address: WAVAXFallsTrancheMasterAddress[NETWORK],
-    abi: AVAXTranchesAbi,
-    masterChefAbi: MasterChefAbi,
-    masterChefAddress: WAVAXFallsMasterWTFAddress[NETWORK],
-    pools: [],
-    depositAssetAddress: WAVAXDepositAddress[NETWORK],
-    depositAssetAddresses: [],
-    depositAssetAbi: WTFAbi,
-    strategyFarms: [
-      {
-        farmName: "Benqi AVAX",
-        shares: 0.5,
-        sAddress: WAVAXBenqiStrategyAddress[NETWORK],
-        apiKey: "qi_avax"
-      },
-      {
-        farmName: "Trader Joe AVAX",
-        shares: 0.5,
-        sAddress: WAVAXTraderJoeStrategyAddress[NETWORK],
-        apiKey: "joe_avax"
-      }
-    ],
-    subgraphURL: "https://api3.waterfalldefi.org/subgraphs/name/waterfall/qiJoe_avax",
-    isRetired: true
-  },
-  {
     portfolio: "BUSD Falls (Autorolled)",
     isAvax: false,
     wrapAvax: false,

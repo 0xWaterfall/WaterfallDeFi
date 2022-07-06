dcff6e5d28966627d26396b4281f3defef472789
0xAngel <0xangel@waterfalldefi.org>
2022-07-07 00:56:48 +0800

    hotfix 2

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -263,8 +263,7 @@ export const usePendingWTFReward = (masterChefAddress: string, trancheCount: num
           params: [account, i]
         });
       }
-      const result =
-        network === "avax" ? await multicall(MasterChefAbi, calls) : await multicallBSC(MasterChefAbi, calls);
+      const result = network === "avax" ? [{ _hex: 0 }] : await multicallBSC(MasterChefAbi, calls);
       let _pendingReward = new BigNumber(0);
       const _tranchesPendingReward = [];
       for (let i = 0; i < result.length; i++) {
